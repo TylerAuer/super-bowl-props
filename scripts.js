ef5235245e821b11ce19721a84e6873e6a19521e
Tyler Auer <Tyler.Auer@gmail.com>
2020-01-31 22:44:37 -0800

    Participant's scores are now calculated
    moved processing of JSON outside of xHTTP request
    
    moved functions that calculate scores outside of Participant constructor function
    
    wrote calculateStats() which returns a JS object. Used in Participant()

--- scripts.js
+++ scripts.js
@@ -1,17 +1,20 @@
 // arrays of cells to access in google sheet
-const APIRanges = { // in A1 notation
-  personalInfo: "Rulings!A4:BE500",
+const APIRanges = {
+  // in A1 notation
   propsList: "Rulings!D1:BE1",
   propsResults: "Rulings!D2:BE2",
+  personalInfo: "Rulings!A4:BE500"
 };
 
 // QUESTION: is this the best way to do this?
 // reduces ranges to single string and formats for API URL
-const ranges = Object.values(APIRanges).map(range => {
-  return "ranges=" + range + "&"
-}).reduce((accumulator, nextVal) => {
-  return accumulator + nextVal
-});
+const ranges = Object.values(APIRanges)
+  .map(range => {
+    return "ranges=" + range + "&";
+  })
+  .reduce((accumulator, nextVal) => {
+    return accumulator + nextVal;
+  });
 
 // combines elements of API URL together
 const APIURL =
@@ -22,16 +25,76 @@ const APIURL =
   "valueRenderOption=FORMATTED_VALUE&key=" +
   APIKey;
 
-// TODO: add method to calculate points
-// TODO: add method to determine percent correct
-// TODO: add method to determine max score
+var xhttp = new XMLHttpRequest();
+
+xhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    data = JSON.parse(xhttp.responseText);
+    processJSONData(data);
+  }
+};
+
+xhttp.open("GET", APIURL, true);
+xhttp.send();
+
+let participantList = [];
+let propsList;
+let propsResults;
+
+// process the ranges from APIRanges
+function processJSONData(data) {
+  // ranges indexes are determined by their order in the APIRanges object
+
+  propsList = data.valueRanges[0].values[0];
+  propsResults = data.valueRanges[1].values[0];
+
+  // Processes participants
+  data.valueRanges[2].values.forEach((data, index) => {
+    const p = new Participant(data[0], data[1], data.slice(3, 57));
+    participantList.push(p);
+    //document.getElementById('participant-data').appendChild(p.HTMLTableRow())
+  });
+
+  console.log("Participants", participantList);
+  // console.log("Props List", propsList);
+  // console.log("Prop results", propsResults);
+}
+
+// Given an array of a Participants picks, calculates points for correct picks
+function calculateStats(picksArray) {
+  let points = 0;
+  let numCorrect = 0;
+  let numWrong = 0;
+  let maxScoreAddOn = 0;
+
+  for (let i = 0; i < picksArray.length; i++) {
+    if (picksArray[i] === propsResults[i]) {
+      // regex needed because some results are worth different numbers of digits
+      points += parseInt(picksArray[i].match(/\((\d+)\s/)[1]);
+      numCorrect += 1;
+    } else if (propsResults[i] !== "null") {
+      numWrong += 1;
+    } else {
+      maxScoreAddOn += parseInt(picksArray[i].match(/\((\d+)\s/)[1]);
+    }
+  }
+
+  const stats = {
+    points: points,
+    numCorrect: numCorrect,
+    numWrong: numWrong,
+    percentCorrect: Math.round((1000 * numCorrect) / picksArray.length) / 10,
+    pointsPerCorrect: Math.round((10 * points) / numCorrect) / 10,
+    maxPossibleScore: points + maxScoreAddOn
+  };
+  return stats;
+}
+
 function Participant(name, handle, picksArray) {
   this.name = name;
   this.handle = handle;
   this.picksArray = picksArray;
-  // this.points = points;
-  // this.percentCorrect = percentCorrect;
-  // this.rank = rank;
+  this.stats = calculateStats(this.picksArray);
   // TODO: Move this method to function outside of Participant object
   // this.HTMLTableRow = () => {
 
@@ -52,7 +115,6 @@ function Participant(name, handle, picksArray) {
   //     }
   //     cell.innerHTML = data;
 
-
   //     tableRow.appendChild(cell);
   //   };
 
@@ -60,45 +122,3 @@ function Participant(name, handle, picksArray) {
 
   // };
 }
-
-let participantList = [];
-let propsList = [];
-let propsResults = [];
-
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function () {
-  if (this.readyState == 4 && this.status == 200) {
-
-    data = JSON.parse(xhttp.responseText);
-
-    // processes the rows for each participant
-    data.valueRanges.forEach(rangeGroup => {
-      if (rangeGroup.range === APIRanges.personalInfo) {
-        rangeGroup.values.forEach((data, index) => {
-          const p = new Participant(data[0], data[1], data.slice(3, 57));
-          participantList.push(p);
-          //document.getElementById('participant-data').appendChild(p.HTMLTableRow())
-        });
-
-        // collects all of the prop titles
-      } else if (rangeGroup.range === APIRanges.propsList) {
-        rangeGroup.values.forEach(data => {
-          propsList.push(data);
-        });
-
-        // collects all of the prop results
-      } else if (rangeGroup.range === APIRanges.propsResults) {
-        rangeGroup.values.forEach(data => {
-          propsResults.push(data);
-        });
-      }
-    });
-  }
-};
-
-xhttp.open("GET", APIURL, true);
-xhttp.send();
-
-console.log(participantList);
-console.log(propsList);
-console.log(propsResults);
